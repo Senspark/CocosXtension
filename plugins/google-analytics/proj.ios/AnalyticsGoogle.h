5d2fd4fd5e2f96f409a393658a271684445b559f
Duc Nguyen <ndpduc@gmail.com>
2015-07-08 17:37:48 +0700

    - Add google analytics 3.2

---
+++ plugins/google-analytics/proj.ios/AnalyticsGoogle.h
@@ -0,0 +1,14 @@
+//
+//  AnalyticsGoogle.h
+//  PluginGoogleAnalytics
+//
+//  Created by Duc Nguyen on 7/7/15.
+//  Copyright (c) 2015 Senspark Co., Ltd. All rights reserved.
+//
+
+#ifndef __PluginGoogleAnalytics__AnalyticsGoogle__
+#define __PluginGoogleAnalytics__AnalyticsGoogle__
+
+#include <stdio.h>
+
+#endif /* defined(__PluginGoogleAnalytics__AnalyticsGoogle__) */
---
+++ plugins/google-analytics/proj.ios/AnalyticsGoogle.m
@@ -0,0 +1,9 @@
+//
+//  AnalyticsGoogle.cpp
+//  PluginGoogleAnalytics
+//
+//  Created by Duc Nguyen on 7/7/15.
+//  Copyright (c) 2015 Senspark Co., Ltd. All rights reserved.
+//
+
+#include "AnalyticsGoogle.h"
