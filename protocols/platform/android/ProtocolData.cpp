25caa0599144721e49fcc92c5f4808d53cab47c1
Duc Nguyen <ndpduc@gmail.com>
2015-08-26 16:23:10 +0700

    - Implement PrototolData on android.

---
+++ protocols/platform/android/ProtocolData.cpp
@@ -0,0 +1,116 @@
+//
+//  GooglePlayProtocolData.m
+//  PluginSenspark
+//
+//  Created by Duc Nguyen on 8/10/15.
+//  Copyright (c) 2015 Senspark Co., Ltd. All rights reserved.
+//
+
+#include "ProtocolData.h"
+#include "PluginJniHelper.h"
+#include <android/log.h>
+#include "PluginUtils.h"
+#include "PluginJavaData.h"
+
+namespace cocos2d { namespace plugin {
+
+    ProtocolData::ProtocolData()
+    : _listener(NULL)
+    {
+    }
+
+    ProtocolData::~ProtocolData()
+    {
+    }
+
+    void ProtocolData::configDeveloperInfo(TDataDeveloperInfo devInfo)
+    {
+        if (devInfo.empty())
+        {
+            PluginUtils::outputLog("ProtocolData", "The developer info is empty for %s!", this->getPluginName());
+            return;
+        }
+        else
+        {
+            PluginJavaData *pData = PluginUtils::getPluginJavaData(this);
+            PluginJniMethodInfo t;
+            if (PluginJniHelper::getMethodInfo(t, pData->jclassName.c_str(), "configDeveloperInfo", "(Ljava/util/Hashtable;)V")) {
+                jobject obj_map = PluginUtils::createJavaMapObject(&devInfo);
+
+                t.env->CallVoidMethod(pData->jobj, t.methodID, obj_map);
+                t.env->DeleteLocalRef(obj_map);
+                t.env->DeleteLocalRef(t.classID);
+            }
+        }
+    }
+
+    void ProtocolData::openData(const std::string &fileName, cocos2d::plugin::DataConflictPolicy policy) {
+        PluginJavaData *pData = PluginUtils::getPluginJavaData(this);
+        PluginJniMethodInfo t;
+        if (PluginJniHelper::getMethodInfo(t, pData->jclassName.c_str(), "openData", "(Ljava/lang/String;int)V")) {
+            jstring file_name = t.env->NewStringUTF(fileName.c_str());
+
+            t.env->CallVoidMethod(pData->jobj, t.methodID, file_name, policy);
+
+            t.env->DeleteLocalRef(file_name);
+            t.env->DeleteLocalRef(t.classID);
+        }
+    }
+
+    void ProtocolData::readCurrentData() {
+        PluginJavaData *pData = PluginUtils::getPluginJavaData(this);
+        PluginJniMethodInfo t;
+
+        if (PluginJniHelper::getMethodInfo(t, pData->jclassName.c_str(), "readCurrentData", "()V")) {
+           t.env->CallVoidMethod(pData->jobj, t.methodID);
+
+           t.env->DeleteLocalRef(t.classID);
+        }
+    }
+
+    void ProtocolData::resolveConflict(const std::string& conflictId, void* data, long length, std::map<std::string, std::string>& changes) {
+        PluginJavaData *pData = PluginUtils::getPluginJavaData(this);
+        PluginJniMethodInfo t;
+
+        if (PluginJniHelper::getMethodInfo(t, pData->jclassName.c_str(), "resolveConflict", "(Ljava/lang/String;[BLjava/util/Hashtable;)V")) {
+           jstring conflict_id = t.env->NewStringUTF(conflictId.c_str());
+           jbyteArray arr = t.env->NewByteArray(length);
+           t.env->SetByteArrayRegion(arr, 0, length, (jbyte*)data);
+
+           jobject obj_changes = PluginUtils::createJavaMapObject(&changes);
+
+           t.env->CallVoidMethod(pData->jobj, t.methodID, conflict_id, arr, obj_changes);
+
+           t.env->DeleteLocalRef(obj_changes);
+           t.env->DeleteLocalRef(conflict_id);
+           t.env->DeleteLocalRef(arr);
+           t.env->DeleteLocalRef(t.classID);
+        }
+    }
+
+    void ProtocolData::commitData(void *data, long length, const char *imagePath, const std::string &description) {
+        PluginJavaData *pData = PluginUtils::getPluginJavaData(this);
+        PluginJniMethodInfo t;
+
+        if (PluginJniHelper::getMethodInfo(t, pData->jclassName.c_str(), "commitData", "([BLjava/lang/String;Ljava/lang/String;)V")) {
+           jbyteArray arr = t.env->NewByteArray(length);
+           t.env->SetByteArrayRegion(arr, 0, length, (jbyte*)data);
+
+           jstring image_path = nullptr;
+           if (imagePath) {
+               image_path = t.env->NewStringUTF(imagePath);
+           }
+
+           jstring desc = t.env->NewStringUTF(description.c_str());
+
+           t.env->CallVoidMethod(pData->jobj, t.methodID, arr, image_path, desc);
+
+           if (image_path) {
+               t.env->DeleteLocalRef(image_path);
+           }
+           t.env->DeleteLocalRef(desc);
+           t.env->DeleteLocalRef(arr);
+           t.env->DeleteLocalRef(t.classID);
+        }
+    }
+}} // namespace cocos2d { namespace plugin {
