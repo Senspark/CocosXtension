2ff8378c3a3180ab8d67a24098f3bb7e1f39ada9
Duc Nguyen <ndpduc@gmail.com>
2015-09-05 17:19:04 +0700

    - Update BaaS, Data protocol.

--- protocols/proj.android/src/org/cocos2dx/plugin/InterfaceData.java
+++ protocols/proj.android/src/org/cocos2dx/plugin/InterfaceData.java
@@ -6,6 +6,6 @@ public interface InterfaceData {
 	public void configDeveloperInfo(Hashtable<String, String> devInfo);
 	public void openData(String fileName, int dataConflictPolicy);
 	public void readCurrentData();
-	public void resolveConflict(String conflictId, Byte[] data, Hashtable<String, String> changes);
-	public void commitData(Byte[] data, String imagePath, String description);
+	public void resolveConflict(String conflictId, byte[] data, Hashtable<String, String> changes);
+	public void commitData(byte[] data, String imagePath, String description);
 }
