f63c646c51a04282b550c15dcc868f669bd2ba69
Duc Nguyen <ndpduc@gmail.com>
2015-12-21 16:50:11 +0700

    - Add one more callback method.

--- protocols/platform/ios/ShareWrapper.h
+++ protocols/platform/ios/ShareWrapper.h
@@ -38,6 +38,7 @@ typedef enum {
 }
 
 + (void) onShareResult:(id) obj withRet:(int) ret withContent: (NSDictionary*) content withMsg:(NSString*) msg;
++ (void) onShareResult:(id) obj withRet:(int) ret withContent: (NSDictionary*) content withMsg:(NSString*) msg andCallbackID: (long) cbID;
 
 + (UIViewController *)getCurrentRootViewController;
 @end
