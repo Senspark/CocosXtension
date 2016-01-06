da9fa0ce1d3316a93ecfe6789a294d2c3afeb4ac
Duc Nguyen <ndpduc@gmail.com>
2016-01-06 11:54:41 +0700

    - Add Platform Protocol

---
+++ protocols/platform/ios/ProtocolPlatform.mm
@@ -0,0 +1,10 @@
+//
+//  ProtocolPlatform.m
+//  PluginProtocol
+//
+//  Created by Duc Nguyen on 1/5/16.
+//  Copyright © 2016 zhangbin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+//#include "ProtocolPlatform.h"
