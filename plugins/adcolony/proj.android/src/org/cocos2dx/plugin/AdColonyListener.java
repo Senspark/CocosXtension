9766f378316d9d385d76fd6a651f52327b4ddcff
Nikel Arteta <nikelarteta@Senspark-Mini-04s-Mac-mini.local>
2016-01-14 10:39:20 +0700

    - New implementation of PluginAdColony.

--- plugins/adcolony/proj.android/src/org/cocos2dx/plugin/AdColonyListener.java
+++ plugins/adcolony/proj.android/src/org/cocos2dx/plugin/AdColonyListener.java
@@ -10,17 +10,31 @@ public class AdColonyListener implements AdColonyAdAvailabilityListener, AdColon
 
 	@Override
 	public void onAdColonyAdAttemptFinished(AdColonyAd ad) {
+		if (ad.shown()) {
+			AdsWrapper.onAdsResult(AdsAdColony.mAdapter, AdsWrapper.RESULT_CODE_AdsShown, "AdColony Interstitial Ad shown");
+		} else if (ad.notShown()) {
+			AdsWrapper.onAdsResult(AdsAdColony.mAdapter, AdsWrapper.RESULT_CODE_UnknownError, "AdColony ad not show with error UNKNOWN");
+		} else if (ad.skipped()) {
+			AdsWrapper.onAdsResult(AdsAdColony.mAdapter, AdsWrapper.RESULT_CODE_AdsDismissed, "AdColony ad DISMISS");		
+		}
 	}
 
 	@Override
 	public void onAdColonyAdStarted(AdColonyAd ad) {
+		AdsWrapper.onAdsResult(AdsAdColony.mAdapter, AdsWrapper.RESULT_CODE_VideoShown, "AdColony started showing");
 	}
 
 	@Override
 	public void onAdColonyV4VCReward(AdColonyV4VCReward ad) {
+		if (ad.success()) {
+			AdsWrapper.onAdsResult(AdsAdColony.mAdapter, AdsWrapper.RESULT_CODE_VideoCompleted, "AdColony V4VC succeeded");
+		} else {
+			AdsWrapper.onAdsResult(AdsAdColony.mAdapter, AdsWrapper.RESULT_CODE_VideoDismissed, "AdColony V4VC dismissed");
+		}
 	}
 
 	@Override
 	public void onAdColonyAdAvailabilityChange(boolean ad, String arg1) {
+		AdsWrapper.onAdsResult(AdsAdColony.mAdapter, AdsWrapper.RESULT_CODE_VideoReceived, "AdColony video received");
 	}
 }
