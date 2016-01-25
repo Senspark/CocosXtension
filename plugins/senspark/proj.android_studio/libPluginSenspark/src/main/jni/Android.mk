58408564061844ccfed5f4c675a45119d67f6e15
Tran Van Tuan <tranvantuan.lak@gmail.com>
2016-01-25 15:07:07 +0700

    fix add pluginSenspark

--- plugins/senspark/proj.android_studio/libPluginSenspark/src/main/jni/Android.mk
+++ plugins/senspark/proj.android_studio/libPluginSenspark/src/main/jni/Android.mk
@@ -2,14 +2,12 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-$(call import-add-path,$(LOCAL_PATH)/../../../..)
-
 LOCAL_MODULE := PluginSensparkStatic
 
 LOCAL_MODULE_FILENAME := libPluginSensparkStatic
 
 LOCAL_SRC_FILES :=\
-$(addprefix ../../platform/android/, \
+$(addprefix ../../../../../platform/android/, \
 	AdColonyProtocolAds.cpp \
     AdmobProtocolAds.cpp \
     ChartboostProtocolAds.cpp \
@@ -27,17 +25,21 @@ $(addprefix ../../platform/android/, \
     GooglePlusProtocolUser.cpp \
     GooglePlusProtocolShare.cpp \
     ParseProtocolBaaS.cpp \
+    BaasboxProtocolBaas.cpp \
 ) \
-../../classes/SensparkPluginManager.cpp
+../../../../../classes/SensparkPluginManager.cpp
 
 LOCAL_CFLAGS := -std=c++11 -Wno-psabi
 LOCAL_EXPORT_CFLAGS := -Wno-psabi
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../include 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../platform/android
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../../include
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../../../platform/android
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../../../../../protocols/include
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../../../../../protocols/platform/android
 
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include 
-LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/../../platform/android
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../../../../include
+LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/../../../../../platform/android
+
 
 LOCAL_LDLIBS := -landroid
 LOCAL_LDLIBS += -llog
@@ -48,4 +50,3 @@ LOCAL_STATIC_LIBRARIES += PluginProtocolStatic
 include $(BUILD_STATIC_LIBRARY)
 
 $(call import-module,android/native_app_glue)
-$(call import-module,protocols/proj.android/jni)
