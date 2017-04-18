ebab3331425e7840c89fd84a05d4003175accc7f
Duc Nguyen <ndpduc@gmail.com>
2017-04-18 14:03:37 +0700

    - Add SSNativeAdvancedAdListener

---
+++ plugins/admob/proj.ios/SSNativeAdvancedAdListener.h
@@ -0,0 +1,13 @@
+//
+//  SSNativeAdvancedAdListener.h
+//  PluginAdmob
+//
+//  Created by Duc Nguyen on 4/17/17.
+//  Copyright © 2017 cocos2d-x. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSNativeAdvancedAdListener : NSObject
+
+@end
---
+++ plugins/admob/proj.ios/SSNativeAdvancedAdListener.m
@@ -0,0 +1,13 @@
+//
+//  SSNativeAdvancedAdListener.m
+//  PluginAdmob
+//
+//  Created by Duc Nguyen on 4/17/17.
+//  Copyright © 2017 cocos2d-x. All rights reserved.
+//
+
+#import "SSNativeAdvancedAdListener.h"
+
+@implementation SSNativeAdvancedAdListener
+
+@end
