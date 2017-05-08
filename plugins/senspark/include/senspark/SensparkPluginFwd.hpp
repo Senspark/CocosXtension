725b55c66e8ea216e78e84fb89e6d9d025a0fdf2
enrevol <enrevol@gmail.com>
2017-05-08 15:14:38 +0700

    Add class forward header.

---
+++ plugins/senspark/include/senspark/SensparkPluginFwd.hpp
@@ -0,0 +1,24 @@
+//
+//  SensparkPluginFwd.hpp
+//  PluginSenspark
+//
+//  Created by Zinge on 5/8/17.
+//  Copyright © 2017 Senspark Co., Ltd. All rights reserved.
+//
+
+#ifndef SENSPARK_PLUGIN_X_SENSPARK_PLUGIN_FWD_HPP_
+#define SENSPARK_PLUGIN_X_SENSPARK_PLUGIN_FWD_HPP_
+
+namespace senspark {
+namespace plugin {
+namespace ads {
+class AdmobProtocolAds;
+} // namespace ads
+
+namespace analytics {
+class GoogleProtocolAnalytics;
+} // namespace analytics
+} // namespace plugin
+} // namespace senspark
+
+#endif /* SENSPARK_PLUGIN_X_SENSPARK_PLUGIN_FWD_HPP_ */
